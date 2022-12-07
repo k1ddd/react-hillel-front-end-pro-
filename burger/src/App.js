9a6dc074f07f5da46aa4c7af312230543a7f4693
vladyslav <vladyslav.klokun@gmail.com>
2022-12-07 23:51:24 +0200

    test2

--- burger/src/App.js
+++ burger/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React выфвфывфв asdadad dasdasdasdsa
+          ABABAGALUMAGA
         </a>
       </header>
     </div>
